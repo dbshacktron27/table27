91e59972eabf9504adc9a14b68deb222348347e0
nmanne1 <naveen.manne32@gmail.com>
2019-08-10 12:19:38 +0530

    Basic Prototype

---
+++ messaging-queue-api/src/main/java/com/dbs/messagingqueue/messagingqueueapi/MessagingQueueApiApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.messagingqueue.messagingqueueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingQueueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingQueueApiApplication.class, args);
+	}
+
+}
